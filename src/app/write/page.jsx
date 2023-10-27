3196a4a311432cd3337810ff769853ebd74c59c1
ngaridennis33 <ngaridennis33@gmail.com>
2023-10-28 00:36:01 +0300

    Updated the react-quil

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,209 +1,3 @@
-// "use client"
-// import React, { useEffect, useState } from 'react'
-// import styles from "./page.module.scss";
-// // import "react-quill/dist/quill.bubble.css";
-// import Image from 'next/image';
-// import { useSession } from 'next-auth/react';
-// // import ReactQuill from 'react-quill';
-// import 'react-quill/dist/quill.snow.css';
-// import { useRouter } from 'next/navigation';
-// import Spinner from '@/components/spinner/spinner';
-// import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-// import { app } from '@/utils/firebase';
-
-// import dynamic from 'next/dynamic'
-// import parse from 'html-react-parser';
-
-//     const QuillNoSSRWrapper = dynamic(import('react-quill'), {
-//       ssr: false,
-//       loading: () => <p>Loading ...</p>,
-//     })
-
-
-//     const modules = {
-//       toolbar: [
-//         [{ header: '1' }, { header: '2' }, { font: [] }],
-//         [{ size: [] }],
-//         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-//         [
-//           { list: 'ordered' },
-//           { list: 'bullet' },
-//           { indent: '-1' },
-//           { indent: '+1' },
-//         ],
-//         ['link', 'image', 'video'],
-//         ['clean'],
-//       ],
-//       clipboard: {
-//         // toggle to add extra line breaks when pasting HTML:
-//         matchVisual: false,
-//       },
-//     }
-//     /*
-//      * Quill editor formats
-//      * See https://quilljs.com/docs/formats/
-//      */
-//     const formats = [
-//       'header',
-//       'font',
-//       'size',
-//       'bold',
-//       'italic',
-//       'underline',
-//       'strike',
-//       'blockquote',
-//       'list',
-//       'bullet',
-//       'indent',
-//       'link',
-//       'image',
-//       'video',
-//     ]
-
-
-// const storage = getStorage(app);
-
-// const WritePage = () => {
-//     // Router to redirect
-//     const router = useRouter();
-//     const {status} = useSession()
-
-  
-//     const [open,setOpen] = useState(false);
-//   const [file, setFile] = useState(null);
-//   const [media, setMedia] = useState("");
-//   const [title, setTitle] = useState("");
-//   const [value,setValue] = useState("");
-//   const [catSlug,setCatSlug] = useState("");
-
-//   // Transform the title into a slug
-//   const slugify = (str) => {
-//     return str
-//       .toLowerCase()
-//       .trim()
-//       .replace(/[^a-z0-9 -]/g, '')   // Remove characters that are not alphanumeric, hyphens, or spaces
-//       .replace(/\s+/g, '-')          // Replace spaces with hyphens
-//       .replace(/-+/g, '-');          // Replace multiple hyphens with a single hyphen
-//   };
-//   const handleSubmit = async()=>{
-//     const res = await fetch("/api/posts",{
-//       method:"POST",
-//       body: JSON.stringify({
-//         title,
-//         desc:value,
-//         img:media,
-//         slug:slugify(title),
-//         catSlug:catSlug,
-//       })
-//     })
-
-//     console.log(res)
-//   }
-
-//   useEffect(()=>{
-//     const upload=()=>{
-//       const name = new Date().getTime + file.name
-//     const storageRef = ref(storage, name);
-
-//     const uploadTask = uploadBytesResumable(storageRef, file);
-
-//     uploadTask.on('state_changed', 
-//       (snapshot) => {
-//         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//         console.log('Upload is ' + progress + '% done');
-//         switch (snapshot.state) {
-//           case 'paused':
-//             console.log('Upload is paused');
-//             break;
-//           case 'running':
-//             console.log('Upload is running');
-//             break;
-//         }
-//       }, 
-//       (error) => {
-//       }, 
-//       () => {
-//         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-//           setMedia(downloadURL);
-//         });
-//       }
-//     );
-
-//         };
-
-//     file && upload();
-//   },[file])
-
-//   if(status === "loading"){
-//     return (
-//     <div className={styles.loading}><Spinner/></div>
-//     )
-//   };
-
-//   if(status === "unauthenticated"){
-//     return (
-//     router.push("/")
-//     )
-//   };
-
-
-//   return (
-//     <div className={styles.container}>
-//        <input
-//         type="text"
-//         placeholder="Title"
-//         className={styles.input}
-//         onChange={(e) => setTitle(e.target.value)}
-//       />
-//       <select className={styles.select} onChange={(e) => setCatSlug(e.target.value)}>
-//         <option value="style">tech</option>
-//         <option value="fashion">fashion</option>
-//         <option value="food">finance</option>
-//         <option value="culture">travel</option>
-//         <option value="travel">sports</option>
-//         <option value="coding">art</option>
-//       </select>
-
-//         <div className={styles.editor}>
-//             <button className={styles.button} onClick={()=> setOpen(!open)}>
-//                 <Image src = "/plus.svg" width={16} height={16} alt ="" />
-//             </button>
-//             {open && (
-//                 <div className={styles.add}>
-//                   <input type="file" id="image" onChange={(e)=> setFile(e.target.files[0])} style={{display:"none"}} />
-//                     <button className={styles.addButton}>
-//                     <label htmlFor="image">
-//                         <Image src = "/images.svg" width={25} height={25} alt ="" className={styles.icon} />
-//                     </label>
-//                         <span className={styles.tooltip}>Images</span>
-//                     </button>
-//                     <button className={styles.addButton}>
-//                         <Image src = "/new-tab.svg" width={25} height={25} alt ="" className={styles.icon}/>
-//                         <span className={styles.tooltip}>New tab</span>
-
-//                     </button>
-//                     <button className={styles.addButton}>
-//                         <Image src = "/play.svg" width={25} height={25} alt ="" className={styles.icon}/>
-//                         <span className={styles.tooltip}>Video</span>
-
-//                     </button>
-//                 </div>
-//             )}
-//             {/* <ReactQuill className={styles.textArea} theme="bubble" value={value} onChange={setValue} placeholder="Tell your story" /> */}
-//             <QuillNoSSRWrapper className={styles.textArea}  modules={modules} placeholder='compose here' value={value} onChange={setValue} formats={formats} theme="snow"  />
-
-// ``<p>{value}</p>
-//     {parse(value)}
-
-//         </div>
-
-//         <button className={styles.publish} onClick={handleSubmit}>Publish</button>
-//     </div>
-//   )
-// }
-
-// export default WritePage
-
 "use client"
 import React, { useEffect, useState } from 'react';
 import styles from './page.module.scss';
@@ -214,7 +8,6 @@ import Spinner from '@/components/spinner/spinner';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { app } from '@/utils/firebase';
 import dynamic from 'next/dynamic';
-import parse from 'html-react-parser';
 
 // Import react-quill styles
 import 'react-quill/dist/quill.snow.css';
@@ -369,7 +162,7 @@ const WritePage = () => {
             </button>
           </div>
         )}
-        <QuillNoSSRWrapper
+        {/* <QuillNoSSRWrapper
           className={styles.textArea}
           modules={modules}
           placeholder="Compose here"
@@ -377,7 +170,7 @@ const WritePage = () => {
           onChange={setValue}
           formats={formats}
           theme="snow"
-        />
+        /> */}
       </div>
       <button className={styles.publish} onClick={handleSubmit}>
         Publish
